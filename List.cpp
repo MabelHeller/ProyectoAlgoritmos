b4395b54a6a86cbd2d82ec9504c12aae9f66b268
Heller <arguedas3023@gmail.com>
2017-05-28 03:40:01 -0600

    Mensaje

---
+++ List.cpp
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   List.cpp
+ * Author: heller
+ * 
+ * Created on May 27, 2017, 3:32 AM
+ */
+
+#include "List.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+List::List() {
+}
+
+List::List(const List& orig) {
+}
+
+List::~List() {
+}
+
+
+
---
+++ List.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   List.h
+ * Author: heller
+ *
+ * Created on May 27, 2017, 3:32 AM
+ */
+
+#ifndef LIST_H
+#define LIST_H
+
+class List {
+public:
+    List();
+    List(const List& orig);
+    virtual ~List();
+private:
+
+};
+
+#endif /* LIST_H */
+
